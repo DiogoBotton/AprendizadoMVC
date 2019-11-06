d345e955dcc37219e27c48f372c4174a45a523cd
DiogoBotton <diogo.luo@gmail.com>
2019-11-06 18:27:56 -0200

    .

---
+++ JornalOnlineMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JornalOnlineMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
